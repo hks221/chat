09a4ef6bc0675788d09526174460ff93bbfc27ba
hks221 <harshsharma041221@gmail.com>
2023-03-23 18:01:47 +0530

    REST API completed

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 //jshint esversion:6
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
+var mongoose = require("mongoose");
 const PORT = 5000;
 const {MONGOURI} = require('./keys');
 
@@ -24,12 +24,21 @@ app.use(
 
  require('./models/user');
  require("./models/recipe");
+ require("./models/ChatModel");
+ require("./models/MessageModel");
+ 
  app.use(express.json());
+
+//  import messageroute from './routes/messageroute'
+ const messageroute =require("./routes/messageroute");
+
 
  app.use(require('./routes/auth'));
  app.use(require('./routes/recipe'));
  app.use(require('./routes/user'));
- 
+ app.use(require('./routes/ChatRoute'));
+//  app.use(require('./routes/messageroute'));
+ app.use('/message',messageroute) 
 
 app.listen(PORT,()=>{
     console.log("Server is running at port 5000");
